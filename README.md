1063b843b220f20160410ee5d750ce900af03510
Patrick R <patrixr@Patricks-MacBook-Pro.local>
2019-04-08 12:06:16 +0800

    copy pasta

---
+++ README.md
@@ -0,0 +1,49 @@
+# Vue reactive clock plugin
+
+
+## What is it good for ?
+
+This plugin helps build time reactive UIs
+
+A few examples where it might be useful :
+
+- If you want to display a clock that updates as time goes by
+- If you have a live dashboard with the current day's info, when passing to the next day, you'll want it to automatically update.
+
+## Usage:
+
+Hook up the plugin to your Vue
+
+```javascript
+
+import Clock from 'vue-reactive-clock'
+
+Vue.use(Clock);
+
+```
+
+This will add a `$clock` property to your Vue which self updates.
+
+It can be used directly in the templates ...
+
+```
+<template>
+ <div>
+    Today's date : {{ $clock.now | formatFilter }}
+ </div>
+</template>
+```
+
+You can use `$clock` programatically in your computed properties to have your UI update as time passes
+
+```
+computed: {
+  isUrgent() {
+    return this.$clock.now.getHours() > 18;
+  }
+}
+```
+
+
+
+
---
+++ index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+
+const ClockPlugin = {
+  install(_Vue) {
+
+    const vm = new Vue({
+      data() {
+        return {
+          now: new Date()
+        }
+      },
+      computed: {
+        time() {
+          return `${this.now.getHours()}:${this.now.getHours()}`;
+        },
+        dateString() {
+          return this.now.toDateString();
+        },
+        startOfDay() {
+          let start = new Date(this.now);
+          start.setHours(0,0,0,0);
+          return start;
+        },
+        endOfDay() {
+          var end = new Date(this.now);
+          end.setHours(23,59,59,999);
+          return end;
+        }
+      },
+      methods: {
+        update() {
+          this.now = new Date();
+        }
+      },
+      created() {
+        setTimeout(() => {
+            this.update();
+            setInterval(() => this.update(), minute);
+          },
+          (60 - this.now.seconds()) * second // seconds to 60
+        );
+      }
+    });
+
+    _Vue.prototype.$clock = vm;
+  }
+};
+
+export default ClockPlugin;
